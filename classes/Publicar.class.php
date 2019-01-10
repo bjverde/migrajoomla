80c8d02c5ac60f8ff1d22b9cec65b2b1bcc4f0ff
Reinaldo Araujo Barreto Junior <reinaldo.junior@mpdft.mp.br>
2019-01-10 21:37:14 -0200

    melhoria das mensgem

--- classes/Publicar.class.php
+++ classes/Publicar.class.php
@@ -17,18 +17,15 @@ class Publicar {
 	        $qtd      = count($listIdsArtigos);
 	        $qtdOK    = count($listArtigosOK);
 	        $qtdFalha = count($listArtigosFalha);
-	        $result   = 'Qtd de artigos solicitados:'.$qtd.'; Qtd de artigos NÃO atualizados:'.$qtdFalha.'. Lista dos id com atualizados: '.$idFalhas;
+	        $result   = 'Qtd de artigos solicitados:'.$qtd
+	                 .'; Qtd de artigos NÃO atualizados:'.$qtdFalha
+	                 .'; Qtd de artigos atualizados:'.$qtdOK;
+	                 //.'. Lista dos id com atualizados: '.$idFalhas;
 	    }
 	    return $result;
 	}
 	//--------------------------------------------------------------------------------
 	public static function artigosAtualizarPublicacao( $idmin, $idmax ){
-	    if( !is_numeric($idmin) ){
-	        throw new DomainException('Valor Min não é inteiro');
-        }
-        if( !is_numeric($idmax) ){
-	        throw new DomainException('Valor Max não é inteiro');
-        }
 	    if( $idmin > $idmax ){
 	        throw new DomainException('Valor Max deve ser maior que o Min');
         }
@@ -36,17 +33,19 @@ class Publicar {
         $artigosStateJ1 = ContentDAO::selectSelectStates( $idmin, $idmax );
         $artigosStateJ3 = J3ContentDAO::selectSelectStates( $idmin, $idmax );
         
-        $listIdsArtigos   = $artigosStateJ3;
-	    $listArtigosFalha = array();
+        $listIdsArtigos   = $artigosStateJ3['J3_ID'];
+	    $listArtigosIguais= array();
 	    $listArtigosOK    = array();
 	    foreach ($artigosStateJ3['J3_ID'] as $key => $value) {
-	        if( $value ==  $artigosStateJ1['ID'][$key] ){
-	            $listArtigosFalha[]=$value;
+	        $idIgual    = ( $artigosStateJ3['J3_ID'][$key] === $artigosStateJ1['ID'][$key] );
+	        $stateIgual = ( $artigosStateJ3['J3_STATE'][$key] === $artigosStateJ1['STATE'][$key] );
+	        if( $idIgual && $stateIgual){
+	            $listArtigosIguais[]=$value;
 	        }else{
 	            $listArtigosOK[]=$value;
 	        }
 	    }
-	    $result = self::msgAcaoMateriaSucesso($listIdsArtigos,$listArtigosOK,$listArtigosFalha);
+	    $result = self::msgAcaoMateriaSucesso($listIdsArtigos,$listArtigosOK,$listArtigosIguais);
 	    return $result;
 	}
 }
