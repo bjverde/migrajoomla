8107e357ce2dfaba845ee6061bc672f5fddf9bd8
Reinaldo Araujo Barreto Junior <reinaldo.junior@mpdft.mp.br>
2019-12-02 20:17:31 -0300

    :bug: listando o ultima arquivo

--- modulos/artigos_publicar.php
+++ modulos/artigos_publicar.php
@@ -4,12 +4,12 @@
 $frm = new TForm('Atualizar status Publicação dos Artigos');
 $frm->setFlat(true);
 $frm->setMaximize(true);
+$controllersRelatorios = new Relatorios();
+$ultimoArtigo = $controllersRelatorios->getUltimosArtigosJ3();
+$MAXID = $ultimoArtigo['ID'][0];
 
-$ultimoArtigo = J3ContentDAO::getUltimoArtigo();
-$MAXID = $ultimoArtigo['J3_ID'][0];
-
-$frm->addHtmlField('aviso','Informa os Ids do intervalo dos artigos. O campo embranco irá rodar sobre todos os artigos, o ultimo artigo no Joomla 3 é '.$MAXID.'. Para atulizar um artigo informe os dois campois com o mesmo valor');
-$frm->addGroupField('gpf01','Lista de Artigos no Joomla 3');
+$frm->addHtmlField('aviso','Informa os Ids do intervalo dos artigos. O campo embranco irá rodar sobre todos os artigos, o ultimo artigo no '.J39.' é '.$MAXID.'. Para atulizar um artigo informe os dois campois com o mesmo valor');
+$frm->addGroupField('gpf01','Lista de Artigos no '.J25);
     $frm->addNumberField('IDMIN', 'Id menor Artigo',8,false,0,null,null,null,null,false);
     $frm->addNumberField('IDMAX', 'Id maior Artigo',8,false,0,false,null,null,$MAXID,false);
 $frm->closeGroup();
