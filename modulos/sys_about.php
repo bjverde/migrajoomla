60d680b10407cc1670c359e3ace711ec04b3cf05
Reinaldo Araujo Barreto Junior <reinaldo.junior@mpdft.mp.br>
2019-12-03 19:55:46 -0300

    :bug: info banco

--- modulos/sys_about.php
+++ modulos/sys_about.php
@@ -24,17 +24,17 @@ $frm->addGroupField('gpx1', 'Sistema');
     $frm->addTextField('versionFormDin', 'Versão do FormDin:', 20, false, 20, FORMDIN_VERSION)->setReadOnly(true);
 $frm->closeGroup();
 
-$frm->addGroupField('gpx3', 'Banco Joomla 1.5.14 - origem dos dados');    
+$frm->addGroupField('gpx3', 'Banco '.J25.' - origem dos dados');    
     $frm->addTextField('servidorBD', 'Servidor de Banco:', 20, false, 20, HOST)->setReadOnly(true);
     $frm->addTextField('banco', 'Banco:', 20, false, 20, DATABASE)->setReadOnly(true);
 $frm->closeGroup();
 
-$frm->addGroupField('gpx2', 'Banco Joomla 3.8.X - Destino dos dados');
-    $perfilBancoAcesso  = ServidorConfig::getInstancia()->getPerfilAcesso();
+$frm->addGroupField('gpx2', 'Banco '.J39.' - Destino dos dados');
+    $perfilBancoAcesso  = ServidorConfig::getInstancia()->getPerfilJ39();
     $frm->addTextField('servidorBDJ3', 'Servidor:', 50, false, 50, $perfilBancoAcesso['HOST'])->setReadOnly(true);
     $frm->addTextField('bancoJ3', 'banco:', 50, false, 50, $perfilBancoAcesso['DATABASE'])->setReadOnly(true);
     $frm->addTextField('usuarioJ3', 'Usuário:', 50, false, 50, $perfilBancoAcesso['USERNAME'])->setReadOnly(true);
-    $frm->addTextField('senhaJ3', 'Senha:', 50, false, 50, $perfilBancoAcesso['PASSWORD'])->setReadOnly(true);
+    //$frm->addTextField('senhaJ3', 'Senha:', 50, false, 50, $perfilBancoAcesso['PASSWORD'])->setReadOnly(true);
 $frm->closeGroup();
 
 
